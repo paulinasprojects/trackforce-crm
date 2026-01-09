af437c855f6dabca6601ae4bb1e1f393c3d4b4ac
paulinasprojects <virtualpaula93@gmail.com>
2026-01-09 23:40:14 +0200

    added header and mobile nav

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,6 +1,35 @@
+import { useState } from "react";
+import MobileNav from "./mobile-nav"
+import { Link } from "react-router-dom";
+
 const Header = () => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleOpen = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div>Header</div>
+    <>
+      <div className="hidden md:flex items-center justify-between text-white  md:mx-5 lg:mx-[40px] xl:mx-[80px] mt-[32px]">
+        <div>
+          <img src="/trackforce-logo.svg" alt="" />
+        </div>
+        <div className="flex items-center gap-6">
+          <Link to="/product" className="hover-underline-animation font-medium font-inter text-[16px] leading-[150%]">Product</Link>
+          <Link to="/changelog" className="hover-underline-animation font-medium font-inter text-[16px] leading-[150%]">Changelog</Link>
+          <Link to="/pricing" className="hover-underline-animation font-medium font-inter text-[16px] leading-[150%]">Pricing</Link>
+          <Link to="/contact" className="hover-underline-animation font-medium font-inter text-[16px] leading-[150%]">Contact Sales</Link>
+        </div>
+        <div className="flex items-center gap-6">
+          <button className="font-medium text-sm font-inter leading-[150%]">Sign in</button>
+          <button className="button-gradient text-white px-6 py-2.5 rounded-md font-medium text-sm font-inter leading-[150%]">Start for Free</button>
+        </div>
+      </div>
+      <div className="block md:hidden">
+        <MobileNav isOpen={open} onToggle={handleOpen} />
+      </div>
+    </>
   )
 }
 
--- src/components/common/mobile-nav.tsx
+++ src/components/common/mobile-nav.tsx
@@ -1,6 +1,63 @@
-const MobileNav = () => {
+import { MdMenu } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
+import { Link } from "react-router-dom";
+import { motion, AnimatePresence } from "framer-motion";
+
+const menuVariants = {
+  hidden: {
+    x: "100%",
+  },
+  visible: {
+    x: 0,
+  },
+  exit: {
+    x: "100%",
+    transition: { duration: 1.5 }
+  },
+};
+
+const MobileNav = ({ isOpen, onToggle }: { isOpen: boolean; onToggle: () => void; }) => {
   return (
-    <div>MobileNav</div>
+    <div className="flex justify-between items-center px-4 mt-5">
+      <a href="/">
+        <img src="/trackforce-logo.svg" alt="" />
+      </a>
+      <button className="cursor-pointer" onClick={onToggle}>
+        {isOpen ?
+          <IoMdClose className="size-5 fixed top-5 right-4 z-[99] text-white cursor-pointer" />
+          : <MdMenu className="size-6 cursor-pointer text-white" />
+        }
+      </button>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div className="fixed inset-0 z-50 bg-black/80"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <motion.div className="bg-[#0e1219] w-full h-screen"
+              variants={menuVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              transition={{
+                type: "spring",
+                stiffness: 100,
+                damping: 20,
+              }}
+            >
+              <div className="flex flex-col gap-12 items-center justify-center pt-[50px] text-white">
+                <Link className="hover-underline-animation" onClick={onToggle} to="/product">Product</Link>
+                <Link className="hover-underline-animation" onClick={onToggle} to="/changelog">Changelog</Link>
+                <Link className="hover-underline-animation" onClick={onToggle} to="/pricing">Pricing</Link>
+                <Link className="hover-underline-animation" onClick={onToggle} to="/contact">Contact Sales</Link>
+              </div>
+
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
   )
 }
 
