7170943aa3d72df3dc19f8954ddfd2fa57ca9a6f
paulinasprojects <virtualpaula93@gmail.com>
2026-02-02 11:22:26 +0200

    added product image + social proof sections

---
+++ src/components/product/product-image.tsx
@@ -0,0 +1,9 @@
+const ProductImage = () => {
+  return (
+    <div className="xl:mx-20 lg:mx-10 md:mx-5 mx-6 lg:mt-14 md:mt-10 mt-8">
+      <img src="/product-image.png" className="w-full" alt="" />
+    </div>
+  )
+}
+
+export default ProductImage
--- src/pages/product-page.tsx
+++ src/pages/product-page.tsx
@@ -1,9 +1,13 @@
+import SocialProof from "../components/home-page/social-proof"
 import ProductHeading from "../components/product/product-heading"
+import ProductImage from "../components/product/product-image"
 
 const ProductPage = () => {
   return (
     <>
       <ProductHeading />
+      <ProductImage />
+      <SocialProof />
     </>
   )
 }
